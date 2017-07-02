2aed7ab7ad55c573cf570bbe1ec44882b756dac7
arze1 <v@dw.gl>
2017-07-02 15:42:49 -0400

    fixed haschild

--- index.js
+++ index.js
@@ -1,111 +1,111 @@
 function getType(data) {
-    if(typeof(data) === "object") {
-        if(data.constructor === Array) return "array";
-        else return "object";
-    } else if(typeof(data) === "number") return "number";
-    else {
-        if(typeof(data) === "boolean") return "bool";
-        else return "string";
-    }
+	if(typeof(data) === "object") {
+		if(data.constructor === Array) return "array";
+		else return "object";
+	} else if(typeof(data) === "number") return "number";
+	else {
+		if(typeof(data) === "boolean") return "bool";
+		else return "string";
+	}
 }
 
 class Save {
 
-    constructor() {
-        this.save = {methods:[]};
-        this.returnValue = false;
-    }
+	constructor() {
+		this.save = {methods:[]};
+		this.returnValue = false;
+	}
 
-    test(string) {
+	test(string) {
 
-        if(!string && string !== false) return false;
-        console.log(string)
-        let tmp = init(string);
+		if(!string && string !== false) return false;
+		console.log(string)
+		let tmp = init(string);
 
-        for(var x of this.save.methods) {
-            tmp[x[0]](x[1][0],x[1][1],x[1][2])
-        }
-        return (this.returnValue ? tmp.v() : tmp);
-    }
+		for(var x of this.save.methods) {
+			tmp[x[0]](x[1][0],x[1][1],x[1][2])
+		}
+		return (this.returnValue ? tmp.v() : tmp);
+	}
 
-    setReturn(val) {
-        this.returnValue = val;
-    }
+	setReturn(val) {
+		this.returnValue = val;
+	}
 
-    add(data) {
-        this.save.methods.push(data)
-    }
+	add(data) {
+		this.save.methods.push(data)
+	}
 }
 
 const typeRegex = {
-    email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    phone: /^\(*\+*[1-9]{0,3}\)*-*[1-9]{0,3}[-. /]*\(*[2-9]\d{2}\)*[-. /]*\d{3}[-. /]*\d{4} *e*x*t*\.* *\d{0,4}$/
+	email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+	phone: /^\(*\+*[1-9]{0,3}\)*-*[1-9]{0,3}[-. /]*\(*[2-9]\d{2}\)*[-. /]*\d{3}[-. /]*\d{4} *e*x*t*\.* *\d{0,4}$/
 };
 
 class Main {
-    constructor(data) {
+	constructor(data) {
 
-        this.s = data; // current data in object
+		this.s = data; // current data in object
 
-        this.tmpsave  = new Save();
+		this.tmpsave  = new Save();
 
-        this.isValid = true;
+		this.isValid = true;
 
-        if(typeof(data) === "object") {
-            this.p = data;
-            this.path = [];
-        }
+		if(typeof(data) === "object") {
+			this.p = data;
+			this.path = [];
+		}
 
-        this.type = getType(data);
-    }
+		this.type = getType(data);
+	}
 
-    equals(str) {
-       this.tmpsave.add(["equals", [str]]);
-        if (!this.isValid) return this;
+	equals(str) {
+		this.tmpsave.add(["equals", [str]]);
+		if (!this.isValid) return this;
 
-        if(this.s === str) return this;
-        else this.isValid = false;
+		if(this.s === str) return this;
+		else this.isValid = false;
 
-        return this;
-    }
+		return this;
+	}
 
-    inequal(str) {
+	inequal(str) {
 
-       this.tmpsave.add(["inequal", [str]]);
-
-        if (!this.isValid) return this;
-
-        if(this.s !== str) return this;
-        else this.isValid = false;
-
-        return this;
-    }
-
-    is(type) {
-
-       this.tmpsave.add(["is", [type]]);
+		this.tmpsave.add(["inequal", [str]]);
 
 		if (!this.isValid) return this;
 
-       if(typeRegex[type]) {
-		   if (typeRegex[type].test(this.s)) return this;
-		   else {
-		       this.isValid = false;
-		       return this;
-           }
-       }
+		if(this.s !== str) return this;
+		else this.isValid = false;
 
-        if(this.type === type) return this;
-        else this.isValid = false;
+		return this;
+	}
 
-        return this;
-    }
+	is(type) {
 
-    isnt(type) {
+		this.tmpsave.add(["is", [type]]);
 
-       this.tmpsave.add(["isnt", [type]]);
+		if (!this.isValid) return this;
 
-        if (!this.isValid) return this;
+		if(typeRegex[type]) {
+			if (typeRegex[type].test(this.s)) return this;
+			else {
+				this.isValid = false;
+				return this;
+			}
+		}
+
+		if(this.type === type) return this;
+		else this.isValid = false;
+
+		return this;
+	}
+
+	isnt(type) {
+
+		this.tmpsave.add(["isnt", [type]]);
+
+		if (!this.isValid) return this;
 
 		if(typeRegex[type]) {
 			if (typeRegex[type].test(this.s)) {
@@ -114,166 +114,166 @@ class Main {
 			} else return this;
 		}
 
-        if(this.type !== type) return this;
-        else this.isValid = false;
+		if(this.type !== type) return this;
+		else this.isValid = false;
 
-        return this;
-    }
+		return this;
+	}
 
-    length(a,b) {
+	length(a,b) {
 
-       this.tmpsave.add(["length", [a, b]]);
+		this.tmpsave.add(["length", [a, b]]);
 
-        if (!this.isValid) return this;
+		if (!this.isValid) return this;
 
-        let _len = this.s.length;
+		let _len = this.s.length;
 
-        if ((a === -1 ? true : _len >= a) &&
-            (b === -1 ? true : _len <= b)) {
-            return this
-        } else this.isValid = false;
+		if ((a === -1 ? true : _len >= a) &&
+			(b === -1 ? true : _len <= b)) {
+			return this
+		} else this.isValid = false;
 
-        return this;
-    }
+		return this;
+	}
 
-    save(returnV = false) {
-        this.tmpsave.setReturn(returnV);
-        return this.tmpsave;
-    }
+	save(returnV = false) {
+		this.tmpsave.setReturn(returnV);
+		return this.tmpsave;
+	}
 
-    valid() {
-        return this.isValid;
-    }
-    v() {
-        return this.isValid;
-    }
+	valid() {
+		return this.isValid;
+	}
+	v() {
+		return this.isValid;
+	}
 }
 
 class _String extends Main {
-    has(str) {
+	has(str) {
 
-       this.tmpsave.add(["has", [str]]);
+		this.tmpsave.add(["has", [str]]);
 
-        if (!this.isValid) return this;
+		if (!this.isValid) return this;
 
-        if(this.s.indexOf(str) > -1) return this;
+		if(this.s.indexOf(str) > -1) return this;
 
-        else this.isValid = false;
+		else this.isValid = false;
 
-        return this;
-    }
+		return this;
+	}
 }
 
 class _Object extends Main {
 
-    hasChild(child) {
+	hasChild(child) {
 
-       this.tmpsave.add(["hasChild", [child]]);
+		this.tmpsave.add(["hasChild", [child]]);
 
-        if (!this.isValid) return this;
+		if (!this.isValid) return this;
 
-        if(!this.s[data]) return false;
+		if(!this.s[child]) return false;
 
-        return this;
-    }
+		return this;
+	}
 
-    child(data) {
+	child(data) {
 
-       this.tmpsave.add(["child", [data]]);
+		this.tmpsave.add(["child", [data]]);
 
-        if (!this.isValid) return this;
+		if (!this.isValid) return this;
 
-        if(this.s[data] || this.s[data] === false) {
+		if(this.s[data] || this.s[data] === false) {
 
-            this.s = this.s[data];
+			this.s = this.s[data];
 
-            this.path.push(data);
+			this.path.push(data);
 
-            this.type = getType(this.s);
+			this.type = getType(this.s);
 
-        } else this.isValid = false;
-        return this;
-    }
+		} else this.isValid = false;
+		return this;
+	}
 
-    parent() {
+	parent() {
 
-       this.tmpsave.add(["parent", []]);
+		this.tmpsave.add(["parent", []]);
 
-        if (!this.isValid) return this;
+		if (!this.isValid) return this;
 
-        if(this.path.length > 0) {
+		if(this.path.length > 0) {
 
-            this.path.splice(this.path.length,1);
+			this.path.splice(this.path.length,1);
 
-            if(this.path.length === 1) this.s = this.p;
-            else {
-                const path = this.path;
+			if(this.path.length === 1) this.s = this.p;
+			else {
+				const path = this.path;
 
-                let tempObj = this.p;
+				let tempObj = this.p;
 
-                for(let i = 0; i < path.length; i++) {
-                    if(tempObj[path[i]]) tempObj = tempObj[path[i]];
-                    else {
-                        this.isValid = false;
-                    }
-                }
+				for(let i = 0; i < path.length; i++) {
+					if(tempObj[path[i]]) tempObj = tempObj[path[i]];
+					else {
+						this.isValid = false;
+					}
+				}
 
 
-                this.s = tempObj;
-            }
+				this.s = tempObj;
+			}
 
-            this.type = getType(this.s);
+			this.type = getType(this.s);
 
-            return this;
-        } else this.isValid = false;
-        return this;
-    }
+			return this;
+		} else this.isValid = false;
+		return this;
+	}
 }
 
 class _Array extends Main {
-    each(statement) {
+	each(statement) {
 
-       this.save.add(["each", [statement]]);
+		this.save.add(["each", [statement]]);
 
-        if (!this.isValid) return this;
+		if (!this.isValid) return this;
 
-        for(var x of this.s) {
-            if(!statement(x)) this.isValid = false;
-        }
-        return this;
-    }
+		for(var x of this.s) {
+			if(!statement(x)) this.isValid = false;
+		}
+		return this;
+	}
 
-    has(str) {
+	has(str) {
 
-       this.save.add(["has", [str]]);
+		this.save.add(["has", [str]]);
 
-        if (!this.isValid) return this;
+		if (!this.isValid) return this;
 
-        if(this.s.indexOf(str) > -1) return this;
-        else this.isValid = false;
+		if(this.s.indexOf(str) > -1) return this;
+		else this.isValid = false;
 
-        return this;
-    }
+		return this;
+	}
 }
 
 const init = function(data) {
-    switch(data) {
-        case "object":
-            return new _Object();
-            break;
-        case "string":
-            return new _String();
-            break;
-        case "array":
-            return new _Array();
-            break;
-    }
-    if(!data && data !== false) return new Main();
+	switch(data) {
+		case "object":
+			return new _Object();
+			break;
+		case "string":
+			return new _String();
+			break;
+		case "array":
+			return new _Array();
+			break;
+	}
+	if(!data && data !== false) return new Main();
 
-    if(typeof(data) === "object") {
-        if(data.constructor === Array) return new _Array(data);
-        else return new _Object(data);
-    } else return new _String(data);
+	if(typeof(data) === "object") {
+		if(data.constructor === Array) return new _Array(data);
+		else return new _Object(data);
+	} else return new _String(data);
 };
 
 module.exports = init;
